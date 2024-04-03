ffad32626d8f598535ba7fcda569905fddf49389
gsm9678 <32432674+gsm9678@users.noreply.github.com>
2024-04-03 17:45:13 +0900

    DataManager add

---
+++ Assets/DataFormat.cs
@@ -0,0 +1,12 @@
+using System;
+
+[Serializable] // Á÷·ÄÈ­
+public class DataFormat
+{
+    public float Zoom_InOut_Value;
+    public float X_Position_Value;
+    public float Y_Position_Value;
+    public float Rotate_Camera_Value;
+    public float X_Size_Value;
+    public float Y_Size_Value;
+}
